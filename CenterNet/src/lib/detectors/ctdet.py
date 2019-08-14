7fe81b3402c4ad2c40967676cdf2561abea7b2dc
Chun-Han Yao <chun-han.yao@bytedance.com>
2019-08-13 17:33:00 -0700

    update

---
+++ CenterNet/src/lib/detectors/ctdet.py
@@ -0,0 +1,112 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import cv2
+import numpy as np
+from progress.bar import Bar
+import time
+import torch
+import random
+
+from external.nms import soft_nms
+from models.decode import ctdet_decode
+from models.utils import flip_tensor
+from utils.image import get_affine_transform
+from utils.post_process import ctdet_post_process
+from utils.debugger import Debugger
+
+from .base_detector import BaseDetector
+
+
+def plot_one_box(x, img, color=None, label=None, line_thickness=None):
+    # Plots one bounding box on image img
+    tl = line_thickness or round(0.002 * max(img.shape[0:2])) + 1  # line thickness
+    color = color or [random.randint(0, 255) for _ in range(3)]
+    c1, c2 = (int(x[0]), int(x[1])), (int(x[2]), int(x[3]))
+    cv2.rectangle(img, c1, c2, color, thickness=tl)
+    if label:
+        tf = max(tl - 1, 1)  # font thickness
+        t_size = cv2.getTextSize(label, 0, fontScale=tl / 3, thickness=tf)[0]
+        c2 = c1[0] + t_size[0], c1[1] - t_size[1] - 3
+        cv2.rectangle(img, c1, c2, color, -1)  # filled
+        cv2.putText(img, label, (c1[0], c1[1] - 2), 0, tl / 3, [225, 255, 255], thickness=tf, lineType=cv2.LINE_AA)
+
+
+class CtdetDetector(BaseDetector):
+  def __init__(self, opt):
+    super(CtdetDetector, self).__init__(opt)
+  
+  def process(self, images, return_time=False):
+    with torch.no_grad():
+      output = self.model(images)[-1]
+      hm = output['hm'].sigmoid_()
+      wh = output['wh']
+      reg = output['reg'] if self.opt.reg_offset else None
+      if self.opt.flip_test:
+        hm = (hm[0:1] + flip_tensor(hm[1:2])) / 2
+        wh = (wh[0:1] + flip_tensor(wh[1:2])) / 2
+        reg = reg[0:1] if reg is not None else None
+      torch.cuda.synchronize()
+      forward_time = time.time()
+      dets = ctdet_decode(hm, wh, reg=reg, K=self.opt.K)
+      
+    if return_time:
+      return output, dets, forward_time
+    else:
+      return output, dets
+
+  def post_process(self, dets, meta, scale=1):
+    dets = dets.detach().cpu().numpy()
+    dets = dets.reshape(1, -1, dets.shape[2])
+    dets = ctdet_post_process(
+        dets.copy(), [meta['c']], [meta['s']],
+        meta['out_height'], meta['out_width'], self.opt.num_classes)
+    for j in range(1, self.num_classes + 1):
+      dets[0][j] = np.array(dets[0][j], dtype=np.float32).reshape(-1, 5)
+      dets[0][j][:, :4] /= scale
+    return dets[0]
+
+  def merge_outputs(self, detections):
+    results = {}
+    for j in range(1, self.num_classes + 1):
+      results[j] = np.concatenate(
+        [detection[j] for detection in detections], axis=0).astype(np.float32)
+      if len(self.scales) > 1 or self.opt.nms:
+         soft_nms(results[j], Nt=0.5, method=2)
+    scores = np.hstack(
+      [results[j][:, 4] for j in range(1, self.num_classes + 1)])
+    if len(scores) > self.max_per_image:
+      kth = len(scores) - self.max_per_image
+      thresh = np.partition(scores, kth)[kth]
+      for j in range(1, self.num_classes + 1):
+        keep_inds = (results[j][:, 4] >= thresh)
+        results[j] = results[j][keep_inds]
+    return results
+
+  def debug(self, debugger, images, dets, output, scale=1):
+    detection = dets.detach().cpu().numpy().copy()
+    detection[:, :, :4] *= self.opt.down_ratio
+    for i in range(1):
+      img = images[i].detach().cpu().numpy().transpose(1, 2, 0)
+      img = ((img * self.std + self.mean) * 255).astype(np.uint8)
+      pred = debugger.gen_colormap(output['hm'][i].detach().cpu().numpy())
+      debugger.add_blend_img(img, pred, 'pred_hm_{:.1f}'.format(scale))
+      debugger.add_img(img, img_id='out_pred_{:.1f}'.format(scale))
+      for k in range(len(dets[i])):
+        if detection[i, k, 4] > self.opt.center_thresh:
+          debugger.add_coco_bbox(detection[i, k, :4], detection[i, k, -1],
+                                 detection[i, k, 4], 
+                                 img_id='out_pred_{:.1f}'.format(scale))
+
+  def show_results(self, debugger, image, results):
+    #debugger.add_img(image, img_id='ctdet')
+    colors = [[random.randint(0, 255) for _ in range(3)] for _ in range(self.num_classes)]
+    for j in range(1, self.num_classes + 1):
+      for bbox in results[j]:
+        if bbox[4] > self.opt.vis_thresh:
+          label = '%f' % (j-1)
+          plot_one_box(bbox[:4], image, label=label, color=colors[j % len(colors)])
+          #debugger.add_coco_bbox(bbox[:4], j - 1, bbox[4], img_id='ctdet')
+    #debugger.show_all_imgs(pause=self.pause)
+    cv2.imwrite('output.png', image)
